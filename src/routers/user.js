5795e7a29efd2187f130431cb923f8819a84fd6f
ramigb <ramigoual@gmail.com>
2020-05-01 11:43:09 +0100

    adding crs-bypass

--- src/routers/user.js
+++ src/routers/user.js
@@ -19,8 +19,8 @@ router.post('/users', async (req, res) => {
     const profileDist = join(__dirname, '../helpers/default_avatar.jpg')
     const backgroundDist = join(__dirname, '../helpers/background.jpeg')
 
-    user.imgUrl = `https://saqsini.herokuapp.com/users/${user._id}/profilePicture`;
-    user.backgroundUrl = `https://saqsini.herokuapp.com/users/${user._id}/backgroundPicture`;
+    user.imgUrl = `https://cors-anywhere.herokuapp.com/https://saqsini.herokuapp.com/users/${user._id}/profilePicture`;
+    user.backgroundUrl = `https://cors-anywhere.herokuapp.com/https://saqsini.herokuapp.com/users/${user._id}/backgroundPicture`;
 
 
     user.profilePict = await sharp(profileDist)
